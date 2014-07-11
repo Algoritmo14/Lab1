df22aabd81bd66bc30d612ea9de64351042e4620
Javier Bucaro <Javier Bucaro@Javier>
2014-07-10 18:33:29 -0600

    new commit

---
+++ src/radio/Radio.java
@@ -0,0 +1,28 @@
+/*
+ * Universidad del Valle de Guatemala 
+ * Algoritmos y Estructura de Datos
+ * Max de Leon
+ * Henry Orellana
+ * Javier Bucaro 13033
+ * Radio.java implementara los metodos para la creacion de una radio
+ */
+
+package radio;
+
+public interface Radio {
+    
+    public void apagar();
+    public void encender();
+    public boolean getEncendido();
+    
+    public void cambiarFrecuencia(boolean frecuencia);
+    public boolean getFrecuencia();
+    
+    public double subirEstacion();
+    public double bajarEstacion();
+    public double getEstacionAM();
+    public double getEstacionFM();
+    
+    public void guardar(int boton,double estacion);
+    public void sacar(int boton); 
+}
---
+++ src/radio/RadioP.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package radio;
+
+/**
+ *
+ * @author Javier Bucaro
+ */
+public class RadioP {
+    private boolean frecuencia, guardar;
+    private double estacionAm, estacionFm;
+    private int boton;
+    
+    public RadioP(){
+        frecuencia = true;
+        guardar = true;
+        estacionAm = 530;
+        estacionFm = 87.9;
+        boton = 0;
+    }
+    public void cambiarFrecuencia(boolean frecuencia){
+        if (frecuencia == true){
+            
+        }
+    }
+}
+    
+
